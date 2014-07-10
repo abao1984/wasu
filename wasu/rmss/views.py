fc1711b30661dd293a1c2155714354b5e4542b2d
guangyao <cgy8888@gmail.com>
2014-07-10 09:09:17 +0800

    add font awesome and bootstrap css
    Signed-off-by: guangyao <cgy8888@gmail.com>

--- wasu/rmss/views.py
+++ wasu/rmss/views.py
@@ -6,7 +6,7 @@ def machine_room_view(request):
     pass
 
 def machine_room_list_view(request):
-    return render_to_response('machine_room_list.html',{},context_instance=RequestContext(request)) 
+    return render_to_response('base.html',{},context_instance=RequestContext(request)) 
 
 def machine_room_add_view(request):
     pass
