d6bb5771ca499d5c9d37c10871fdcef2077d4c57
caoguangyao <cgy8888@gmail.com>
2014-08-08 13:17:57 +0800

    add test case and remove boss info in settings.py

--- wasu/rmss/tests.py
+++ wasu/rmss/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
+from rmss.models import *
 # Create your tests here.
+
+class SimpleTestCase(TestCase):
+    def test_plus(self):
+        self.assertEqual(1+1, 2)
